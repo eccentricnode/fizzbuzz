bf62fe8238fefb482fdf375e6905e2e2a7603d56
eccentricnode <shival.bigman@gmail.com>
2021-09-09 20:39:33 -0700

    created the layout, and set up controller function. began working on logic function

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-// Hello world
-function helloWorld(){
-    alert('Hello World');
+// logic function 
+function getValues() {
+   let fizzValue = document.getElementById("fizzValue").value;
+   let buzzValue = document.getElementById("buzzValue").value;
+   let numbers = [];
+
+   fizzValue = parseInt(fizzValue);
+   buzzValue = parseInt(buzzValue);
+
+   if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+       numbers = generateFizzBuzz(fizzValue, buzzValue);
+
+       viewData(numbers)
+   } else {
+       alert("You must only enter numbers");
+   }
+}
+
+// controller function 
+function generateFizzBuzz(fizzValue, buzzValue) {
+    let numbers = []
+
+    // for(let i = 1; i = 100; i++) {
+    //     if(startValue) {
+
+    //     }
+    // }
+
+    // return numbers;
+}
+
+// view function 
+function viewData() {
+
 }
