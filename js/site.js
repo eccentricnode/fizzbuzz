58b4b2119caefd6002142f3d8580cacdbe664e0c
eccentricnode <shival.bigman@gmail.com>
2021-09-11 22:13:40 -0700

    incremental checkin

--- js/site.js
+++ js/site.js
@@ -9,8 +9,9 @@ function getValues() {
 
    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
        numbers = generateFizzBuzz(fizzValue, buzzValue);
+       console.log("2", numbers);
 
-       viewData(numbers)
+       viewData(numbers);
    } else {
        alert("You must only enter numbers");
    }
@@ -20,16 +21,52 @@ function getValues() {
 function generateFizzBuzz(fizzValue, buzzValue) {
     let numbers = []
 
-    // for(let i = 1; i = 100; i++) {
-    //     if(startValue) {
-
-    //     }
-    // }
-
-    // return numbers;
+    for(let i = 1; i <= 100; i++) {
+        if (i % fizzValue == 0 && i % buzzValue == 0) {
+            numbers.push("FizzBuzz");
+        }
+        else if (i % fizzValue == 0) {
+            numbers.push("Fizz");
+        }
+        else if (i % buzzValue == 0) {
+            numbers.push("Buzz");
+        }
+        else {
+            numbers.push(i);
+        }
+    }
+    return numbers;
 }
 
 // view function 
-function viewData() {
+function viewData(numbers) {
 
+    let tableBody = document.getElementById("results");
+    let templateRow = document.getElementById("fizzBuzzData")
+
+    // clear the table first
+    tableBody.innerHTML = "";
+
+    for(let i = 0; i < numbers.length; i+=5) {
+        const tableRow = document.importNode(templateRow.content, true);
+
+        rowCols = tableRow.querySelectorAll("td");
+        
+        rowCols[0].classList.add(numbers[i]);
+        rowCols[0].textContent = numbers[i];
+        
+        rowCols[1].classList.add(numbers[i+1]);
+        rowCols[1].textContent = numbers[i + 1];
+        
+        rowCols[2].classList.add(numbers[i+2]);
+        rowCols[2].textContent = numbers[i + 2];
+        
+        rowCols[3].classList.add(numbers[i+3]);
+        rowCols[3].textContent = numbers[i + 3];
+        
+        rowCols[4].classList.add(numbers[i+4]);
+        rowCols[4].textContent = numbers[i + 4];
+        
+        tableBody.appendChild(tableRow);
+    }
 }
